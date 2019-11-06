2ff3ab48d0a63e0e270599f5d1d06bffa223a92f
JimHazeR <53271607+JimHazeR@users.noreply.github.com>
2019-11-06 18:45:22 +0900

    add files that we can control motor
    we can control motor via serial monitor by using this sketch

---
+++ serial_motor_control/serial_motor_control.ino
@@ -0,0 +1,40 @@
+int i = 0;
+char buf[20];
+int ch[5];
+
+const int kdegree = 1;
+const int kspeed = 1;
+
+int rightP = 0;
+int leftP = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+ if (Serial.available()) {
+    buf[i] = Serial.read();
+    if (buf[i] == 'e') {
+      buf[i] = '\0';
+      //Serial.println(buf);
+
+      ch[0] = atoi(strtok(buf, ","));
+      ch[1] = atoi(strtok(NULL, ","));
+
+      Serial.println(ch[0]);
+      Serial.println(ch[1]);
+      
+      rightP = (ch[0]*kdegree)+(ch[1]*kspeed);
+      leftP = -1*((ch[0]*kdegree))+(ch[1]*kspeed);
+
+      Serial.println(rightP);
+      Serial.println(leftP);
+      i = 0;
+    }
+    else {
+      i++;
+    }
+  }
+  
+}
