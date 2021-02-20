aa306ba13b97d5a805586f5583cfbceda48406eb
JimHazeR <hajime.nishimura0502@gmail.com>
2021-02-20 18:20:03 +0900

    changed structure

---
+++ include/p_tb67h.h
@@ -0,0 +1,20 @@
+#ifndef POLOLU_TB67H420FTG_DRIVE_H_
+#define POLOLU_TB67H420FTG_DRIVE_H_
+
+#include <Arduino.h>
+
+class p_tb67h{
+public:
+    p_tb67h(uint8_t in1, uint8_t in2, byte pwm, bool lo1, bool lo2);
+    void on(int power);
+    void stop();
+
+private:
+    uint8_t _in1;
+    uint8_t _in2;
+    byte _pwm;
+    bool _lo1;
+    bool _lo2;
+};
+
+#endif
